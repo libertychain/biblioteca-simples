31d404c41e964456c156ac047884037f5d797910
libertychain <libertychain1@gmail.com>
2025-11-16 17:37:41 -0300

    Deploy otimizado

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,7 @@ const app = require('./app');
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
+// OBRIGATÓRIO: escutar em 0.0.0.0 para containers
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
